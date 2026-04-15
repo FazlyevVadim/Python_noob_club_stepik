cbf6249f80bafdcecb4b7c76f7a7fba732ce1942
Rustam Islakaev <rislakaev@gmail.com>
2026-04-15 14:24:17 +0300

    Capter 2 > part 2 in progress

--- Python_noob_club.py
+++ Python_noob_club.py
@@ -236,3 +236,151 @@ else:
     print(pices)
 
 # Степик 2.2 (Операторы break/continue)
+#break - оператор завершения цикла
+#continue - оператор перехода к следующей итерации цикла(если она есть)
+
+#Задача 1
+i = 0
+s = 0
+while i < 10:
+    i = i + 1
+    s = s + i
+    if s > 15:
+        break
+    i = i + 1
+# какое зачение будет иметь i, после завершения цикла( то есть сработает break) i = 7
+
+#Задача 2
+i = 0
+s = 0
+while i < 10:
+    i = i + 1
+    s = s + i
+    if s > 15:
+        continue
+    i = i + 1
+# определите, какое значение будет иметь переменная i после выполнения следующего фрагмента программы i = 10
+# обратить внимане, когда срабатывает continue, i = i + 1 не работает и цикл начинается сверху
+
+#Задача 3
+while True:
+    p = int(input('Вводим числа: '))
+    if p < 10:
+        continue
+    elif 10 <= p <= 100:
+        print(p)
+    else:
+        break
+#12 4 2 58 112/// 12 58
+#101 - пусто
+#1 2 102 - пусто
+
+# Степик 2.3 (Цикл for)
+
+for i in 1,2,3:
+    print(i * i) # 1/ 4/ 9
+for i in range(10):
+    print(i * i) # 0/ 1/ 4/ 9/ 16 и тд
+# range(5):0, 1, 2, 3 ,4 (не включая 5)
+# range(2, 5): 2, 3 ,4 (не включая 5) с шагом 1
+# range(2, 15, 4): 2, 6, 10, 14 - все чсла от 2 до 15 (не включая 15) с шагом 4
+# range(5+1): 0, 1, 2, 3, 4,5 (включая 5 так как +1)
+# range(start=0, to, step=1) start  - по умолчанию 0, шаг по умполчанию - 1, to - обязательная
+# Вывести квадрат из звездочек
+n = int(input())
+for i in range(n):
+    print('*' * n)
+# Вывести квадрат из звездочек с вложенным циклом
+#n = int(input())
+for i in range(n): #Количество строк
+    for j in range(n): #Количество символов в строке
+        print('*', end='')
+    print()
+
+#Задача 1
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+for i in range(c, d + 1):
+    print('\t',i, end='')
+print()
+for j in range(a, b + 1):
+    print(j, end='')
+    for g in range(c, d +1 ):
+        print('\t', j * g, end='')
+    print()
+
+#Вывксти сумму всех нечетных числе от А - Б включая обе границы
+#На вход изет цифра 3 и 7 , результат 15 ( 3+ 5 + 7)
+#Решение 1
+a,b = input('Введите 2 числа: ').split()
+a = int(a)
+b = int(b)
+s = 0 #переменная для накопления чисел
+for i in range(a, b+1):
+    if i % 2 ==1:
+        s += i
+print(s)
+#решение 2
+a,b = input('Введите 2 числа: ').split()
+a = int(a)
+b = int(b)
+s = 0 #переменная для накопления чисел
+if a % 2 == 0:
+    a += 1
+for i in range(a, b+1, 2):
+    s += i
+print(s)
+#Решение 3
+a, b = (int(i) for i in input().split()) #
+s = 0
+if a % 2 == 0:
+    a += 1
+for i in range(a, b + 1, 2):
+    s += i
+print(s)
+
+#Задача 2
+a = int(input('Первое число: '))
+b = int(input('Второе число: '))
+len = 0
+sum = 0
+for i in range(a, b + 1):
+    if i % 3 == 0:
+        sum = sum + i
+        len = len + 1
+print(sum/len)
+
+# Степик 2.4 (Строки и символы)
+#genome = 'ATGG'
+#genome[0] A genome[-1] G
+#genome[1] T genome[-2] G
+#genome[2] G genome[-3] T
+#genome[3] G genome[-4] A
+#i = 1, print(genome[1]) = T
+genome = 'ATGG'
+for i in genome:
+    print(i)
+#Найти кол-во С в строке
+genome = input()
+pint(genome.count('C'))
+
+#s = 'aTGcc'  p = 'cc'
+#s.upper() - все буквы делает большими
+#s.lower() - все буквы делает маленькими
+#s.count(p) - кол-во пар сс
+#s.find(p) - ищет вхождение по индексу
+#s.find('A') - -1, так как нет подходящих букв в строке
+#s.replace('c','C') - aTGCC, заменить маленькие на большие
+s = 'agTtcAGtc'
+s.upper().count('gt'.upper()) #результат 2
+
+#Задача 1
+programm = input()
+programmUp = programm.upper()
+upG = programmUp.count('G')
+upC = programmUp.count('C')
+lengs = len(programmUp)
+print(((upG + upC) / lengs) * 100)
+
