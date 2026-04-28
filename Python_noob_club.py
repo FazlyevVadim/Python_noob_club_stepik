ed411336b7de6ac4c0ddb6f585094f331993e68e
Rustam Islakaev <rislakaev@gmail.com>
2026-04-28 18:40:18 +0300

    Capter 2 > 2.6 home work > Lesson 5 Done Start chapter 3.1

--- Python_noob_club.py
+++ Python_noob_club.py
@@ -669,3 +669,31 @@ for row in result:
     print(*row)
 
 #Задача 5
+n = int(input())
+matrix = [[0] * n for i in range(n)]
+num = 1
+top, bottom = 0, n - 1
+left, right = 0, n - 1
+while left <= right and top <= bottom:
+    for i in range(left, right + 1):
+        matrix[top][i] = num
+        num += 1
+    top += 1
+    for i in range(top, bottom + 1):
+        matrix[i][right] = num
+        num += 1
+    right -= 1
+    if top <= bottom:
+        for i in range(right, left - 1, - 1):
+            matrix[bottom][i] = num
+            num += 1
+        bottom -= 1
+    if left <= right:
+        for i in range(bottom, top - 1, - 1):
+            matrix[i][left] = num
+            num += 1
+        left += 1
+for row in matrix:
+    print(*row)
+
+# Степик 3.1 (Функции)
